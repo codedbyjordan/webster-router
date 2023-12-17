91c284979fde396ec1389b7f532794e3ce16beea
Jordan Baron <jordannbaron@gmail.com>
2023-12-17 12:33:05 -0500

    only export router

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import { Elysia } from "elysia";
-import { router } from "./router";
-
-const app = new Elysia().use(router()).listen(3000);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+export { router } from "./router";
